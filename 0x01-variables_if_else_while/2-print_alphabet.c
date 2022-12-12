29d79057cccdc4d2f6151e035df4688a375ae976
Tedros Kahsu <tedroskahsu14@gmail.com>
2022-12-12 02:42:44 +0000

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+
+	return (0);
+}
