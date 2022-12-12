ea8309ef13cc925db3ef90a76da5b5b8a6e8b67d
Tedros Kahsu <tedroskahsu14@gmail.com>
2022-12-12 05:58:32 +0000

    print lowercase in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char a = 'z';
+
+	while (a >= 'a')
+	{
+		putchar(a);
+		a--;
+	}
+	putchar('\n');
+
+	return (0);
+}
