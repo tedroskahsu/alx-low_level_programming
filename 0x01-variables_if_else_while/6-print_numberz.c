d9bdc897841db0c235f802f5299e320204582baa
Tedros Kahsu <tedroskahsu14@gmail.com>
2022-12-12 05:33:59 +0000

    char is int

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: nothing
+ */
+int main(void)
+{
+	int n = 48;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	
+	return (0);
+}
