dfa3791f4917db620e7c3a1dfd7a196f3c4f6f50
Tedros Kahsu <tedroskahsu14@gmail.com>
2023-01-05 07:02:12 +0000

    fix betty error

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _print_rev_recursioin - prints a string in reverse
+ * _print_rev_recursion - prints a string in reverse
  * @s: pointer to string
  *
  * Return: nothing
