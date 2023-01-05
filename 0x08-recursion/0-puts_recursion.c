9259ff96fc7291b9612e964146246199c40c533c
Tedros Kahsu <tedroskahsu14@gmail.com>
2023-01-05 06:41:56 +0000

    print string by recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,11 +8,12 @@
  */
 void _puts_recursion(char *s)
 {
-	if(*s != '\0')
+	if (*s != '\0')
 	{
 		_putchar(*s);
 		s++;
 		_puts_recursion(s);
 	}
-	else _putchar('\n');
+	else
+		_putchar('\n');
 }
