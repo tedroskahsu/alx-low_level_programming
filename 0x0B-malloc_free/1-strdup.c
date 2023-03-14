f47dfaad35b5425db31cd427d94670b9edee4f81
Tedros Kahsu <tedroskahsu14@gmail.com>
2023-03-14 19:32:39 +0000

    Copy string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,5 +10,33 @@
 */
 char *_strdup(char *str)
 {
-	
+	int size = 0;
+	char *ptr = str;
+	char *ptrMalloc;
+
+	if (str == NULL)
+		return (NULL);
+	while (*str != '\0')
+	{
+		size++;
+		str++;
+	}
+
+	size++;
+	str = ptr;
+
+	ptrMalloc = malloc(sizeof(*ptrMalloc) * size);
+
+	if (ptrMalloc == NULL)
+		return (NULL);
+	ptr = ptrMalloc;
+	while (*str != '\0')
+	{
+		*ptrMalloc = *str;
+		ptrMalloc++;
+		str++;
+	}
+	ptrMalloc = ptr;
+	return (ptrMalloc);
+
 }
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -5,6 +5,9 @@
 #include <stdlib.h>
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
-
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+char **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
 
 #endif
